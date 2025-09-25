d71d1d42521f2a79108f24ab02ff9152fb50a86b
Yemichael Ayele <mirth132456@gmail.com>
2025-09-25 10:54:14 +0300

    check

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteSitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
-    theme: {
+    react(),
+  viteSitemap({
+      hostname: 'https://yourdomain.com', // replace with your real domain
+      routes: [
+        '/',              // Home
+        '/about',         // About page
+        '/services',      // Services
+        '/gallery',       // Gallery
+        '/contact'        // Contact
+      ]
+    })
+  ],
+  theme: {
     extend: {
       keyframes: {
         scrollLeft: {
